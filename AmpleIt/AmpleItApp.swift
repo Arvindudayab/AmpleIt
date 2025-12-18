4cf6cb11b3dd3a034ee9cbe6100e07ead2d8fd5f
arvind <audayabanu@umass.edu>
2025-12-17 22:16:02 -0500

    Initial Commit

---
+++ AmpleIt/AmpleItApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmpleItApp.swift
+//  AmpleIt
+//
+//  Created by Arvind Udayabanu on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AmpleItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
